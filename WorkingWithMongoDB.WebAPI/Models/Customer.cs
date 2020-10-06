19bb9a285bb6dfc4ed78c89dd10e5ff05e942eae
Trần Xuân Việt <viet.tx@3si.vn>
2020-10-06 17:04:22 +0700

    Add project files.

---
+++ WorkingWithMongoDB.WebAPI/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithMongoDB.WebAPI.Models
+{
+    public class Customer
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [BsonElement("PhoneNumber")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+    }
+}
